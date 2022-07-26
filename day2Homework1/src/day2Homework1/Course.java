ecf1caab729028f7116399ac15cdee6b94f36c5a
Muhammed Murat Özgür <ozgurmuhammedmurat@gmail.com>
2022-07-26 16:20:30 +0300

    Updated

--- day2Homework1/src/day2Homework1/Course.java
+++ day2Homework1/src/day2Homework1/Course.java
@@ -1,14 +1,15 @@
 package day2Homework1;
 
 public class Course {
-	
-	public Course(int courseId, String courseName, String courseInstructor) {
+
+	/*public Course(int courseId, String courseName, String courseInstructor) {//Yanlýþ kullaným
 		this.courseId = courseId;
 		this.courseName = courseName;
 		this.courseInstructor = courseInstructor;
-	}
-	
+	}*/
+
 	int courseId;
 	String courseName;
 	String courseInstructor;
+	int d;
 }
--- day2Homework1/src/day2Homework1/Main.java
+++ day2Homework1/src/day2Homework1/Main.java
@@ -3,17 +3,27 @@ package day2Homework1;
 public class Main {
 
 	public static void main(String[] args) {
+
+		/*Course course1 = new Course(1, "Java + React", "Engin Demirog");//Yanlış kullanım
+		Course course2 = new Course(2, "JavaScript", "Engin Demirog");*/
 		
-		Course course1 = new Course(1, "Java + React", "Engin Demirog");
-		Course course2 = new Course(2, "JavaScript", "Engin Demirog");
-		Course[] courses = {course1, course2};
+		Course course1 = new Course();
+		course1.courseId = 1;
+		course1.courseName = "Java + React";
+		course1.courseInstructor = "Engin Demirog";
 		
+		Course course2 = new Course();
+		course2.courseId = 2;
+		course2.courseName = "JavaScript";
+		course2.courseInstructor = "Engin Demirog";
+		Course[] courses = { course1, course2 };
+
 		CourseManager courseManager = new CourseManager();
 		courseManager.changeCourseName(course1);
-		
+
 		courseManager.changeCourseInstructor(course2);
-		
-		for(Course course : courses) {
+
+		for (Course course : courses) {
 			System.out.println(course.courseName);
 		}
 	}
